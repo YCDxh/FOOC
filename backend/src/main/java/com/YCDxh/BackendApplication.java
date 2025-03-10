72fde5a72036d8b3ee1ede090e6170ae50b189ac
YCDxh <3363842322@qq.com>
2025-03-10 17:55:46 +0800

    后端更新结构

--- backend/src/main/java/com/YCDxh/BackendApplication.java
+++ backend/src/main/java/com/YCDxh/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.fooc.backend;
+package com.YCDxh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/YCDxh/BackendApplicationTests.java
+++ backend/src/test/java/com/YCDxh/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.fooc.backend;
+package com.YCDxh;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
